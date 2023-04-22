4eb67dd2c1c7acb34d0271f1b9b0ab5b1cb025e2
Ashutosh Kr Singh <ashutoshkrsinghsm@gmail.com>
2023-04-22 10:28:43 +0530

    Added styles to contact-section and tried to change cursor using cotton.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// // init ball
+// const ball = new Cotton('#ball', {
+//     speed: 1
+//   })
+  
+//   // init circle
+//   const circle = new Cotton('#circle', {
+//     models: '.pill-btn',
+//     on: {
+//       enterModel(cotton, model) {
+//         model.textContent = 'ACTIVE'
+//       },
+//       leaveModel(cotton, model) {
+//         model.textContent = 'MODEL'
+//       }
+//     }
+//   })
